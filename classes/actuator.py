3edfc833beb8707f990868ef8874a6ab80742828
karimkohel <kareemkohel@gmail.com>
2023-03-01 19:18:43 +0200

    finished to mouse pointing feature

--- classes/actuator.py
+++ classes/actuator.py
@@ -1,13 +1,80 @@
 import pyautogui
-import os
+import math
 from pyvolume import pyvolume
 
 
 class Actuator():
     """Class containing all main methods of controlling a PC in the HTI context 
     """
-    def __init__(self) -> None:
-        pass
+    def __init__(self, numOfRegions: int = 4) -> None:
+        """Actuator constructor, to manage windows and mouse control
+
+        Parameters
+        ----------
+        numOfRegions : int, required
+            Number of divisions for the main screen, should be 4, 9, 16 and so on
+
+            Regions should be numbered starting from 0 which is the top left corner to bottom right corner,
+            moving horizontally each increment and then start over from next row
+
+        """
+
+        self.numOfRegions = numOfRegions
+        self.regionsCenters = self._getRegionCenters(self.numOfRegions)
+
+    def _getRegionCenters(self, numOfRegions: int) -> list[int]:
+        """private method to get the coordinates of each region's center in the main screen
+
+        Parameters
+        ----------
+        numOfRegions : int, required
+            Number of divisions for the main screen, defaults to 4 regions 
+
+        Returns
+        ----------
+        list[int]: a list containing all regions' centers as int
+            relative to the main screen only, ordered in the the standard order for the regions
+        """
+        self.screenGridSize = int(math.sqrt(self.numOfRegions))
+        self.screenSize = {
+            'width': pyautogui.size().width,
+            'height': pyautogui.size().height
+        }
+        
+        regionWidth = self.screenSize['width'] / self.screenGridSize
+        regionHeight = self.screenSize['height'] / self.screenGridSize
+
+        # center = (x,y)
+        regionCenter = (int(regionWidth/2), int(regionHeight/2))
+
+        centers = []
+        for i in range(self.screenGridSize):
+            heightCenter = regionCenter[1] + (i*regionHeight)
+            for j in range(self.screenGridSize):
+                widthCenter = regionCenter[0] + (j*regionWidth)
+                centers.append((widthCenter, heightCenter))
+
+        return centers
+
+    def pointToRegion(self, regionNum: int) -> None:
+        """Method to drive the mouse to point to the center of a specified region on the screen
+
+        Parameters
+        ----------
+        regionNum : int, required
+            the specific region number to center the mouse at (can be 0,1,2,3... depending on the number of regions) 
+
+        Raises
+        ----------
+        IndexError: if the region number passed was larger than the available regions
+        """
+
+        if regionNum >= self.numOfRegions:
+            raise IndexError(f"Region number is out of range of available regions specified in the class constructor, which is {self.numOfRegions} starting from 0")
+
+        pyautogui.moveTo(self.regionsCenters[regionNum][0], self.regionsCenters[regionNum][1])
+
+        
 
     @staticmethod
     def copy() -> None:
